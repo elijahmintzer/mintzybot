9124e573f9ae1b0ec3765e1d6b11a66fb55bdfa3
elijahmintzer <47578341+elijahmintzer@users.noreply.github.com>
2020-06-27 13:38:26 -0400

    added comments in index.js

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const Discord = require('discord.js');
 require('dotenv-flow').config();
 const client = new Discord.Client();
 
+//Config files
 const config = {
     token: process.env.TOKEN,
     owner: process.env.OWNER,
     prefix: process.env.PREFIX
 }
+
+//When index.js is executed, then the terminal logs that the bot is connected.
+//It will also get the activity of the bot to "I'm being Built!".
 client.on('ready', ()=> {
     console.log('Bot is now connected');
     client.user.setActivity("I'm Being Built!");
 });
 
+//When a user types "!getavatar", the bot gets the avatar of the author.
+// When a user types "!getavatar @user", it will get the avatar of that user.
 client.on('message', message => {
     if(message.content.startsWith('!getavatar')){
         if(!message.mentions.users.size){
@@ -24,6 +30,7 @@ client.on('message', message => {
     }
 });
 
+//When a user joins the server, the bot says "Welcome, @user." in #welcome chat.
 client.on('guildMemberAdd', member => {
     const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome');
 
