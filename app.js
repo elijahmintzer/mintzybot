174922f5caea76f744d07fb775837f02542182e4
elijahmintzer <47578341+elijahmintzer@users.noreply.github.com>
2020-06-27 22:14:39 -0400

    improvised and edited color roles

--- app.js
+++ app.js
@@ -5,20 +5,31 @@ const client = new Discord.Client();
 //Config files
 const config = {
     token: process.env.TOKEN,
-    owner: process.env.OWNER
+    owner: process.env.OWNER,
+    prefix: process.env.PREFIX
 }
 
-const BLUE_ROLE = '726506610578423990';
+//Color Roles
+const BLUE_ROLE = '726512954949894186';
+const RED_ROLE = '726615170948005928';
+const GREEN_ROLE = '726615209409511444';
 
 //When index.js is executed, then the terminal logs that the bot is connected.
 //It will also get the activity of the bot to "I'm being Built!".
 client.on('ready', ()=> {
-    console.log('Bot is now connected');
+    console.log('Bot is now connected to Discord Services.');
+    console.log(`Logged in as ${client.user.username}`);
     client.user.setActivity("I'm Being Built!");
 });
 
 
 client.on('message', message => {
+
+    const prefix = config.prefix;
+    var args = message.content.toLowerCase().split(" ");
+    var cmd = message.content;
+    const member = message.author;
+
 //When a user types "!getavatar", the bot gets the avatar of the author.
 //When a user types "!getavatar @user", it will get the avatar of that user.
     if(message.content.startsWith('!getavatar')){
@@ -30,11 +41,21 @@ client.on('message', message => {
         });
         message.channel.send(avatarList);
     }
-//When a user types "!role blue", it will give them a role that has the color of blue.
-//It will also say that "@user has been given the blue role".
-    if(message.content == '!role blue'){
-        message.member.roles.add(BLUE_ROLE);
-        message.channel.send(`${message.author} has been given the blue role`);
+//When a user types "!color blue", it will give them a role that has the color of blue.
+//It will also say that "@user's color has been added".
+    if(cmd.toLowerCase().startsWith(`${prefix}coloradd`)){
+        if(args[1] === 'blue'){
+            message.member.roles.add(BLUE_ROLE);
+            message.channel.send(`${member}'s color has been added.`);
+        }
+        if(args[1] === 'red'){
+            message.member.roles.add(RED_ROLE);
+            message.channel.send(`${member}'s color has been added.`);
+        }
+        if(args[1] === 'green'){
+            message.member.roles.add(GREEN_ROLE);
+            message.channel.send(`${member}'s color has been added.`);
+        }
     }
 });
 
