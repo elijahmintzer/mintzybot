776bee8eaa1504c20d3e9d1552351d2a50a8a31e
elijahmintzer <47578341+elijahmintzer@users.noreply.github.com>
2020-06-28 20:35:08 -0400

    added "!gag" and "!ungag"

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const config = {
 const BLUE_ROLE = '726512954949894186';
 const RED_ROLE = '726615170948005928';
 const GREEN_ROLE = '726615209409511444';
+const GAGGED = '726865963470815322';
 
 //When index.js is executed, then the terminal logs that the bot is connected.
 //It will also get the activity of the bot to "I'm being Built!".
@@ -29,6 +30,8 @@ client.on('message', message => {
     let args = message.content.toLowerCase().split(" ");
     let cmd = message.content;
     const member = message.author;
+    let user = message.guild.member(message.mentions.users.first());
+    let reason = args.slice(2).join(' ');
 //When a user types "!getavatar", the bot gets the avatar of the author.
 //When a user types "!getavatar @user", it will get the avatar of that user.
     if(cmd.toLowerCase().startsWith(`${prefix}getavatar`)){
@@ -76,7 +79,97 @@ client.on('message', message => {
             message.channel.send(`${member}'s color has been removed.`);
         }
     }
+
+//ADMINISTRATION CMDS
+
+//When an administrator types "!gag @user <reason>" then it will not allow the user to type in text chat.
+//This command is only accessible to users that have the "MUTE_MEMBERS" permission.
+    if(cmd.toLowerCase().startsWith(`${prefix}gag`)){
+        if(message.member.hasPermission(`MUTE_MEMBERS`)){
+            if(!user){
+                const embed = new Discord.MessageEmbed()
+                .setColor("FF0000")
+                .setTitle("Error!")
+                .setDescription(`${member}, You need to state the user you wish to gag!`)
+
+                message.channel.send(embed);
+            }
+            if(!reason){
+                const embed = new Discord.MessageEmbed()
+                .setColor("FF0000")
+                .setTitle("Error!")
+                .setDescription(`${member}, You need to state a reason for the gag!`)
+
+                message.channel.send(embed);
+            }
+//Checks to see if user already has "gagged" role. If user already has gagged role it will return an error.
+            if(user.roles.cache.has(GAGGED)){
+                const embed = new Discord.MessageEmbed()
+                .setColor("FF0000")
+                .setTitle("Error!")
+                .setDescription(`This user is already gagged!`)
+
+                message.channel.send(embed);
+            } else {
+                if(user){
+                    if(reason){
+                        const embed = new Discord.MessageEmbed()
+                        .setColor("FF0000")
+                        .setTitle("User has successfully been gagged")
+                        .setDescription("This user can no longer type in chat.")
+                        .addField(`USER`, `${user}`)
+                        .addField(`ADMIN`, `${member}`)
+                        .addField(`REASON`, `${reason}`)
     
+                        message.channel.send(embed);
+                        user.roles.add(GAGGED);
+                    }
+                }
+            }
+        }
+    }
+
+//When an administrator types "!ungag @user" then it will allow the user to type in text chat.
+//This command is only accessible to users that have the "MUTE_MEMBERS" permission.
+    if(cmd.toLowerCase().startsWith(`${prefix}ungag`)){
+        if(message.member.hasPermission('MUTE_MEMBERS')){
+            if(!user){
+                const embed = new Discord.MessageEmbed()
+                .setColor("FF0000")
+                .setTitle("Error!")
+                .setDescription(`${member}, You need to state the user you wish to ungag!`)
+
+                message.channel.send(embed);
+            }
+        }
+//Checks if user does NOT have "gagged" role. If user does not have role then it will remove it.
+        if(!user.roles.cache.has(GAGGED)){
+            const embed = new Discord.MessageEmbed()
+            .setColor("FF0000")
+            .setTitle("Error!")
+            .setDescription(`This user is not gagged!`)
+
+            message.channel.send(embed);
+        } else {
+            if(user){
+                const embed = new Discord.MessageEmbed()
+                .setColor("00ff0a")
+                .setTitle("User has successfully been ungagged")
+                .setDescription("This user can now type in chat.")
+                .addField(`USER`, `${user}`)
+                .addField(`ADMIN`, `${member}`)
+    
+                message.channel.send(embed);
+                user.roles.remove(GAGGED);
+            }
+        }
+    }
+
+//END OF ADMINISTRATION CMDS
+
+//When a user types "!help" it will show an embedded message that will show the two pages of the help menu.
+//The two pages are "admin" and "user".
+//"!help admin" will show administration commands while "!help user" will show commands that @everyone can do.
     if(cmd.toLowerCase().startsWith(`${prefix}help`)){
 
         if(args[1] === 'user'){
@@ -94,7 +187,7 @@ client.on('message', message => {
             const embed = new Discord.MessageEmbed()
             .setColor("69F5F0")
             .setTitle("MintzyBot - Admin Help Menu")
-            .addField("!gag <@user>", "Stops a user from typing in any text channel.")
+            .addField("!gag <@user> <reason>", "Stops a user from typing in any text channel.")
             .addField("!ungag <@user>", "Allows a user to type in text channels.")
             .addField("!mute <@user>", "Stops a user from using voice.")
             .addField("!unmute <@user>", "Allows a user to talk using voice")
